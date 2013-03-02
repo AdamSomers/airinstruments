df17b1ad58b3db99d76b8ecc360a7f5db636fcf4
Adam Somers <adsomers@gmail.com>
2013-03-02 01:28:04 -0800

    Different path for dropbox resources

--- airdrums/JuceLibraryCode/BinaryData.h
+++ airdrums/JuceLibraryCode/BinaryData.h
@@ -54,12 +54,6 @@ namespace BinaryData
     extern const char*   TMD_CHIL_VERB_aif;
     const int            TMD_CHIL_VERB_aifSize = 1365786;
 
-    extern const char*   button_small_off_png;
-    const int            button_small_off_pngSize = 2940;
-
-    extern const char*   button_small_on_png;
-    const int            button_small_on_pngSize = 2938;
-
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
     const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
