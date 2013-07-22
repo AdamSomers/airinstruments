c49fa6b02347e89a057c3d9210c550e37525b724
Adam Somers <adsomers@gmail.com>
2013-07-21 19:33:08 -0700

    Bump version to 1.0.1 and manually edit plist

--- airdrums/JuceLibraryCode/JuceHeader.h
+++ airdrums/JuceLibraryCode/JuceHeader.h
@@ -39,8 +39,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "AirBeats";
-    const char* const  versionString  = "1.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "1.0.1";
+    const int          versionNumber  = 0x10001;
 }
 
 #endif   // __APPHEADERFILE_LSBNFZ__
