285784602e2b9b2b3a336633b4064e0dd0f28d64
Adam Somers <adsomers@gmail.com>
2013-08-22 11:16:29 -0700

    Bump version to 1.0.5

--- airharp-draft3/JuceLibraryCode/JuceHeader.h
+++ airharp-draft3/JuceLibraryCode/JuceHeader.h
@@ -39,8 +39,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "AirHarp";
-    const char* const  versionString  = "1.0.3";
-    const int          versionNumber  = 0x10003;
+    const char* const  versionString  = "1.0.5";
+    const int          versionNumber  = 0x10005;
 }
 
 #endif   // __APPHEADERFILE_LSBNFZ__
